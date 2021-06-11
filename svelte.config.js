70c5cd000025e3141bde8441df52a6aa2772bcd9
vhollo <vhollo@users.noreply.github.com>
2021-06-11 20:59:16 +0200

    build netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
-import adapter from '@sveltejs/adapter-static'
+//import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-netlify'
 import { mdsvex } from "mdsvex"
 import { mdsvexConfig } from "./mdsvex.config.js"
-//import yaml from '@rollup/plugin-yaml'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: [".svelte", ...mdsvexConfig.extensions],
