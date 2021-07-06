7a92a7aa0e26291ca7a8bd3762d406bab85aa690
vhollo <vhollo@users.noreply.github.com>
2021-07-06 14:56:52 +0200

    netlify-form?

--- src/lib/my/Form.svelte
+++ src/lib/my/Form.svelte
@@ -7,9 +7,8 @@ export let comp
   {#if comp.subtitle}<div><h3>{comp.subtitle}</h3></div>{/if}
   <div>
     <a href="/{comp.lang}{comp.action}" hidden aria-hidden="true">{!!comp.netlify} {comp.name}</a>
-    <form name="{comp.name}" method="POST" action="/{comp.lang}{comp.action}" netlify="{!!comp.netlify}">
-      <input name=form-name type=hidden value="{comp.name}">
-      <!--<input type="hidden" name="form-name" value="{comp.name}">-->
+    <form name="{comp.name}" method="POST" action="/{comp.lang}{comp.action}" data-netlify="{!!comp.netlify}">
+      <input type="hidden" name="form-name" value="{comp.name}">
       {#if comp.text}{@html comp.text}{/if}
       <a href="/{comp.lang}{comp.action}" hidden aria-hidden="true">{comp.name}</a>
     </form>
