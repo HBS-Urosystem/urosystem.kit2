51d5a58b5c3a0abe1ac52dfe78e5fa646995a647
vhollo <now@civilcontrol.org>
2021-10-14 10:15:21 +0200

    no hotjar on /

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,8 @@
 <script context="module">
+  //export const hydrate = true
+  //export const prerender = true
+  //export const prerender = false
+
   import { onMount } from 'svelte'
   import { goto/*, invalidate, prefetch, prefetchRoutes*/ } from '$app/navigation'
   import { sitelang, cookies/*, moved*/ } from '$lib/stores'
@@ -41,7 +45,7 @@
     </script>
 
     <!-- Hotjar Tracking Code for https://www.urosystem.com/ -->
-    <script>
+    <!--<script>
       (function(h,o,t,j,a,r){
           h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
           h._hjSettings={hjid:2464168,hjsv:6};
@@ -50,7 +54,7 @@
           r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
           a.appendChild(r);
       })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    </script>
+    </script>-->
   {/if}
   
   {#if !!$cookies && !dev }
