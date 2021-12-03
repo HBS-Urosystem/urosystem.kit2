ef9b222e7eb38813040ae916ec00833a1c881a01
vhollo <vhollo@protonmail.com>
2021-12-03 18:05:54 +0100

    GTM

--- src/routes/[lang]/__layout.svelte
+++ src/routes/[lang]/__layout.svelte
@@ -114,6 +114,13 @@
     /></noscript>
     <!-- End Facebook Pixel Code -->
     <!--{/if}--> <!-- $moved -->
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-T4KTKF5');</script>
+      <!-- End Google Tag Manager -->
 	{/if} <!-- $cookies -->
 </svelte:head>
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -29,7 +29,7 @@
 
 <svelte:head>
   <link rel="canonical" href="https://urosystem.com/en"/>
-  <meta name='robots' content='follow, noindex,follow' />
+  <meta name='robots' content='follow, noindex' />
   <title>Bladder instillation with UroDapter® urological syringe adapter</title>
   <meta name="description" content="The UroDapter® urological adapter replaces catheter. It enables painless and complication-free bladder treatment for several lower urinary tract diseases.">
   <meta name="keywords" content="instillation, urological adapter, bladder disease, pain-free, cystitis, catheter replacement, painless, complication-free, bladder, treatment">
