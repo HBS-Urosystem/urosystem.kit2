b7b6d0a5b47f2cbba2744f78796eb13e93c867fe
vhollo <vhollo@protonmail.com>
2022-01-19 23:44:43 +0100

    footer: urosystem link

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -20,7 +20,7 @@
     {#if $sitelang == 'hu'}
     <a href="/hu/company#ginop" aria-label="ginop"><img loading="lazy" src="/uploads/ginop.png" alt="GINOP pályázat" style="width:20ch;display:block;"/></a>
     {/if}
-    <a href="/" aria-label="home"><img loading="lazy" src="/uploads/urosystem_logo_02_web.svg" alt="" style="filter:invert();width:20ch;display:block; aspect-ratio: 100 / 90.861"/></a>
+    <a href="https://www.urosystem.com" rel="external" target="_blank" aria-label="urosystem home"><img loading="lazy" src="/uploads/urosystem_logo_02_web.svg" alt="" style="filter:invert();width:20ch;display:block; aspect-ratio: 100 / 90.861"/></a>
     {#if $sitelang == 'hu'}
     <a href="/hu/company#nkfi" aria-label="nkfi"><img loading="lazy" src="/uploads/nkfi.png" alt="NKFI pályázat" style="width:20ch;display:block;"/></a>
     {/if}
