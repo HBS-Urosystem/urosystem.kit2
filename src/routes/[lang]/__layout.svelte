016ec9cee4a2e675e159da639d59e2ec7fcf9178
vhollo <vhollo@protonmail.com>
2022-09-27 11:29:15 +0200

    Active Campaign tracking code

--- src/routes/[lang]/__layout.svelte
+++ src/routes/[lang]/__layout.svelte
@@ -124,8 +124,17 @@
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-T4KTKF5');</script>
+      })(window,document,'script','dataLayer','GTM-T4KTKF5');
+    </script>
       <!-- End Google Tag Manager -->
+    
+    <!-- Active Campaign -->
+    <script>
+      (function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
+      vgo('setAccount', '477728237');
+      vgo('setTrackByDefault', true);
+      vgo('process');
+    </script>
 	{/if} <!-- $cookies -->
   
   {#if _site == '_ud'}
