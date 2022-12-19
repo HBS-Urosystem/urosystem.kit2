6c8f4e2bc35f35e811cc886e580c200254eafb37
vhollo <vhollo@protonmail.com>
2022-12-19 11:32:33 +0100

    rss fix2

--- src/routes/hirlevel/rss.xml.js
+++ src/routes/hirlevel/rss.xml.js
@@ -10,7 +10,7 @@ export async function get() {
   const feed = new RSS({
     title: 'Urosystem Events',
     description: 'We aim to give guidance for individuals who think they may have IC, as well as tips and tricks for those who have already received a diagnosis and are looking for ways to alleviate symptoms. Our company, Urosystem Ltd. is focused on spreading awareness of IC/BPS and encourages you to share our content with anyone who is affected by this disease.',
-    site: 'https://www.urosystem.com/blog/events/',//import.meta.env.SITE,
+    site: 'https://www.urosystem.com/events/',//import.meta.env.SITE,
     site_url: 'https://www.urosystem.com/events',
     feed_url: 'https://www.urosystem.com/events/rss.xml', // This links to our endpoint
     customData: `<language>en-us</language>`,
@@ -26,11 +26,12 @@ export async function get() {
       }),
       //link: `https://www.urosystem.com/events/`,
       //date: item.date1,
-      custom_elements: [
+      enclose: {url: `${item.images?.length && getSanityImageUrl(item.images[0]).width(400).url() || ''}`},
+      /*custom_elements: [
         {'img': `${item.images?.length && getSanityImageUrl(item.images[0]).width(400).url() || ''}`},
         {'date1': `${item.date1 || null}`},
         {'date2': `${item.date2 || null}`},
-      ],
+      ],*/
     })
   }
   return {
