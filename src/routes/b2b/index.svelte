b20c346dff00beedd5d15ada9397f7e635ecd753
vhollo <vhollo@protonmail.com>
2023-01-24 21:31:32 +0100

    goto(`https://b2b.urosystem.com`)

--- src/routes/b2b/index.svelte
+++ src/routes/b2b/index.svelte
@@ -1,4 +1,6 @@
 <script context="module">
+  import { goto/*, invalidate, prefetch, prefetchRoutes*/ } from '$app/navigation'
+  goto(`https://b2b.urosystem.com`, { replaceState: true });
 
 </script>
 
