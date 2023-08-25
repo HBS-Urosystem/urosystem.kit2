2c76715069c0c4d77364dabea823f00c13c3fe5e
vhollo <vhollo@protonmail.com>
2023-08-25 12:24:37 +0200

    gtag conversion

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -34,6 +34,7 @@
 	onMount(() => {
     document.querySelector('html').lang = $state.thislang.id
     document.querySelector('html').dir = $state.thislang.dir
+    //console.log($state.post.id.indexOf('index'))
 	});
 
 </script>
@@ -123,6 +124,11 @@
           });
         </script>
       {/if}
+
+      {#if $state.post.id.indexOf('index') == 0}
+        <!-- Event snippet for Oldalmegtekintés conversion page --> 
+        <script> gtag('event', 'conversion', {'send_to': 'AW-433475160/DN5ECOK9rvwCENic2c4B'}); </script>
+      {/if}
     {:else}
       {#if _site == '_us'}
         <script>
@@ -147,21 +153,7 @@
       
     {/if}
 
-    <!--{#if !!$cookies }-->
-      <!-- Google Tag Manager -->
-      <!--<script>
-        (function(w,d,s,l,i){
-          w[l]=w[l]||[];
-          w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
-          var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-          j.async=true;
-          j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-          f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-T4KTKF5');
-      </script>-->
-        <!-- End Google Tag Manager -->
-      
-      {#if !!$cookies}
+    {#if !!$cookies}
         <!-- Hotjar Tracking Code for https://www.urosystem.com/ -->
       <script>
         (function(h,o,t,j,a,r){
@@ -201,18 +193,18 @@
         vgo('setTrackByDefault', true);
         vgo('process');
       </script>
-    <!--{/if}-->
-  {/if}
+      
+    {/if}
   
-  {#if _site == '_ud' || _site == '_na'}
-    <style>
-      main {
-        background-image: var(--grad-light-blue);
-        background-position: 50% 50%;
-        background-size: cover;
-      }
-    </style>
-  {/if}
+    {#if _site == '_ud' || _site == '_na'}
+      <style>
+        main {
+          background-image: var(--grad-light-blue);
+          background-position: 50% 50%;
+          background-size: cover;
+        }
+      </style>
+    {/if}
   {/if}
 </svelte:head>
 
