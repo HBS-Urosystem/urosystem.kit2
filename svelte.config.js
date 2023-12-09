2ce164cfa1276b0e4b9d16feb5e63ffd04047ccb
vhollo <vhollo@protonmail.com>
2023-12-09 21:23:23 +0100

    htmlMinifierAdaptor

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,14 @@ import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
 import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-netlify";
+import htmlMinifierAdaptor from "sveltekit-html-minifier";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: [".svelte", ...mdsvexConfig.extensions],
 
   kit: {
-    adapter: adapter(),
+    adapter: htmlMinifierAdaptor(adapter()),
     //prerender: {"force": true}
     /*csp:{
       mode:"auto",
