dafda59151abc32049bc1d36201662fc67c10cdf
vhollo <vhollo@protonmail.com>
2024-02-27 20:18:43 +0100

    b2b -> ac preview fix

--- src/lib/Components.svelte
+++ src/lib/Components.svelte
@@ -11,14 +11,14 @@
   import Buttons from '$lib/my/Buttons.svelte'
   import Qas from '$lib/my/Qas.svelte'
   import Form from '$lib/my/Form.svelte'
-  import B2Bmail from '$lib/my/B2Bmail.svelte'
+  //import B2Bmail from '$lib/my/B2Bmail.svelte'
   import ACmail from '$lib/my/ACmail.svelte'
 	import Modal from '$lib/my/Modal.svelte'
 
   const options = {
 		article: Article,
 		form: Form,
-		b2bmail: B2Bmail,
+		//b2bmail: B2Bmail,
 		acmail: ACmail,
 		images: Images,
 		grid: Grid,
