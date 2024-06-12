fb0117b99bcd4ba9bf96686551141ab304607bab
vhollo <vhollo@protonmail.com>
2024-06-12 23:48:53 +0200

    index.html?

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,7 @@ const config = {
   kit: {
     adapter: htmlMinifierAdaptor(
       adapter({
-        fallback: '200.html' // may differ from host to host
+        fallback: 'index.html' // may differ from host to host
       }),
       {
         minifierOptions: {
