7bb240e757c5d1cc15e95a2c0e6b8f13f5e7d2db
vhollo <vhollo@protonmail.com>
2024-06-13 21:11:04 +0200

    form action

--- src/routes/[...path]/thankyou/+page.server.js
+++ src/routes/[...path]/thankyou/+page.server.js
@@ -1,7 +1,25 @@
 //export const prerender = true
 export const actions = {
-  default: async ({request}) => {
-    const data = await request.formData()
-    return  { success: true }
+  default: async ({request, url}) => {
+    const formData = await request.formData()
+    let origin = url.origin;
+		if (url.origin.includes('localhost')) origin = 'https://urosystem.netlify.app';
+
+    try {
+			const response = await fetch(`${origin}/en/contact`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded'
+				},
+				body: new URLSearchParams(formData).toString()
+			});
+
+      const data = await response.text();
+			if (response.status !== 200) return { err: data }
+      return  { success: true }
+		} catch (err) {
+			console.log('error: ', err)
+			return { err: err }
+		}
   }
 }
