bda4ac6eb910960c51e60788abf7028fad7510d6
vhollo <vhollo@protonmail.com>
2024-06-13 09:56:44 +0200

    static->netlify?

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-netlify";
 import htmlMinifierAdaptor from "sveltekit-html-minifier";
 
 /** @type {import('@sveltejs/kit').Config} */
