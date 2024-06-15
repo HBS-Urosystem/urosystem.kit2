64c88112930f5fb93ecc7a1466f5fe5412368ea2
vhollo <vhollo@protonmail.com>
2024-06-15 13:50:42 +0200

    new GTM test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -53,9 +53,13 @@
   <link rel="canonical" href="{variables.siteurl[$state.post.canonical] || _siteurl}/en{!!$state.post.subpage && $state.post.subpage.slug !== '.' ? ('/' + $state.post.subpage.path) : ('/' + $state.post.path || '')}"/>
 
   {#if !dev}
-    <script>
-      // Define dataLayer and the gtag function.
-      window.dataLayer = window.dataLayer || [];
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-T4KTKF5');
+
       function gtag(){dataLayer.push(arguments);}
       gtag('consent', 'default', {
         'ad_storage': 'denied',
@@ -65,16 +69,12 @@
       });
     </script>
     
-    <!-- Google tag (gtag.js) --> 
-    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-433475160"></script>
     {#if _site == '_us'}
       <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
         gtag('config', 'AW-433475160');
-        gtag('config', 'GTM-T4KTKF5');
-        //gtag('config', 'G-KREFNHDNH0');
       </script>
     {/if}
     {#if _site == '_ud'}
@@ -83,8 +83,6 @@
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
         gtag('config', 'AW-433475160');
-        gtag('config', 'GTM-T4KTKF5');
-        //gtag('config', 'G-1SZ43L7KWV');
       </script>
     {/if}
   
